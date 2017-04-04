0e180acb27eca5525c8db304c1124525d2da55e3
Shabbir Ahmed <shabbirahmed149@gmail.com>
2017-04-04 06:53:39 +0000

    Generate controller & view for foods

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -3,11 +3,22 @@ class FoodsController < ApplicationController
   end
 
   def new
+    @food = Food.new
+  end
+  
+  def create
+    
   end
 
   def edit
   end
 
   def show
+  end
+  
+  private
+  
+  def food_params
+    params.require(:food).permit(:title, :description, :cost, :location, :directions, :nutrition, :calorie, :image1, :image2, :image3)
   end
 end
