fdcba6503406562d3294d235c47d0b9fe3dcbfc3
Shabbir Ahmed <shabbirahmed149@gmail.com>
2017-04-04 13:16:58 +0000

    Improved

--- app/assets/javascripts/food_image_upload_preview.js
+++ app/assets/javascripts/food_image_upload_preview.js
@@ -1,53 +1,66 @@
+$(document).ready(function () {
 	$(function() {
-		  function readURL(input) {
-		    if (input.files && input.files[0]) {
-		      var reader = new FileReader();
+	  function readURL(input) {
+	    if (input.files && input.files[0]) {
+	      var reader = new FileReader();
+	
+	      reader.onload = function (e) {
+	        $('#img_prev1').attr('src', e.target.result);
+	      }
+	      reader.readAsDataURL(input.files[0]);
+	    }
+	  }
+	
+	  $("#avatar-upload1").change(function(){
+	    $('#img_prev1').removeClass('hidden');
+	    readURL(this);
+	  });
+	});
 		
-		      reader.onload = function (e) {
-		        $('#img_prev1').attr('src', e.target.result);
-		      }
-		      reader.readAsDataURL(input.files[0]);
-		    }
-		  }
-		
-		  $("#avatar-upload1").change(function(){
-		    $('#img_prev1').removeClass('hidden');
-		    readURL(this);
-		  });
-		});
-		
-		$(function() {
-		  function readURL(input) {
-		    if (input.files && input.files[0]) {
-		      var reader = new FileReader();
-		
-		      reader.onload = function (e) {
-		        $('#img_prev2').attr('src', e.target.result);
-		      }
-		      reader.readAsDataURL(input.files[0]);
-		    }
-		  }
-		
-		  $("#avatar-upload2").change(function(){
-		    $('#img_prev2').removeClass('hidden');
-		    readURL(this);
-		  });
-		});
-		
-		$(function() {
-		  function readURL(input) {
-		    if (input.files && input.files[0]) {
-		      var reader = new FileReader();
-		
-		      reader.onload = function (e) {
-		        $('#img_prev3').attr('src', e.target.result);
-		      }
-		      reader.readAsDataURL(input.files[0]);
-		    }
-		  }
-		
-		  $("#avatar-upload3").change(function(){
-		    $('#img_prev3').removeClass('hidden');
-		    readURL(this);
-		  });
-		});
+	$(function() {
+	  function readURL(input) {
+	    if (input.files && input.files[0]) {
+	      var reader = new FileReader();
+	
+	      reader.onload = function (e) {
+	        $('#img_prev2').attr('src', e.target.result);
+	      }
+	      reader.readAsDataURL(input.files[0]);
+	    }
+	  }
+	
+	  $("#avatar-upload2").change(function(){
+	    $('#img_prev2').removeClass('hidden');
+	    readURL(this);
+	  });
+	});
+	
+	$(function() {
+	  function readURL(input) {
+	    if (input.files && input.files[0]) {
+	      var reader = new FileReader();
+	
+	      reader.onload = function (e) {
+	        $('#img_prev3').attr('src', e.target.result);
+	      }
+	      reader.readAsDataURL(input.files[0]);
+	    }
+	  }
+	
+	  $("#avatar-upload3").change(function(){
+	    $('#img_prev3').removeClass('hidden');
+	    readURL(this);
+	  });
+	});
+	
+	$('#browse1').click(function(){
+	    $('input[id="avatar-upload1"]').trigger('click');
+	});
+	$('#browse2').click(function(){
+	    $('input[id="avatar-upload2"]').trigger('click');
+	});
+	$('#browse3').click(function(){
+	    $('input[id="avatar-upload3"]').trigger('click');
+	});
+	
+});
