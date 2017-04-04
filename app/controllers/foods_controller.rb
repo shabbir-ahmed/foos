70404dbfbf7f08b36bc723f26d35f8f8f12035e8
Shabbir Ahmed <shabbirahmed149@gmail.com>
2017-04-04 07:50:35 +0000

    Add new features

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,5 +1,6 @@
 class FoodsController < ApplicationController
   def index
+    @foods = Food.all
   end
 
   def new
@@ -7,13 +8,22 @@ class FoodsController < ApplicationController
   end
   
   def create
-    
+    params.permit!
+  	@food = current_user.foods.new(food_params)
+  	if @food.save
+  		redirect_to food_path(@food),
+        success: 'Create your delecious food'
+  	else
+  		render 'new',
+        danger: 'Something wrong'
+  	end
   end
 
   def edit
   end
 
   def show
+    @food = Food.find(params[:id])
   end
   
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    has_many :users
+    has_many :foods
     
     has_secure_password
 	validates_confirmation_of :password, :message => "should match confirmation" 
