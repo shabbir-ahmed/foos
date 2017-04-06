f24a69ddc778e83180e27a7880d48051dd4e924d
Shabbir Ahmed <shabbirahmed149@gmail.com>
2017-04-06 13:41:19 +0000

    New

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,11 @@
 module ApplicationHelper
+	# Redirect stored location
     def redirect_back_or(default)
 		redirect_to(session["forwarding_url"] || default)
 		session.delete(:forwarding_url)
 	end
 
+	# Store request url
 	def store_location
 		session[:forwarding_url] = request.url if request.get?
 	end
