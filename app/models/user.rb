dc0990ac5c2970c0e855d26de03dbb0f2aa1a61f
Shabbir Ahmed <shabbirahmed149@gmail.com>
2017-04-07 06:40:00 +0000

    New

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
     has_many :foods
+
+    mount_uploader :image, AvatarUploader
     
     has_secure_password
 	validates_confirmation_of :password, :message => "should match confirmation" 
