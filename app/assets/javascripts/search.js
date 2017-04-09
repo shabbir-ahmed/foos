c7a34eeba31947b91a0753b1021fd7b21adcc875
Shabbir Ahmed <shabbirahmed149@gmail.com>
2017-04-09 06:09:15 +0000

    New

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,4 +1,5 @@
-$(document).ready(function() {
+$(document).on('turbolinks:load', function() {
+// $(document).ready(function() {
     $('#key').on('input', function() {
         var h = $('#key').val();
         if (h.length > 1) {
@@ -13,5 +14,17 @@ $(document).ready(function() {
             $("#s_form").submit();
 
         }
-    });
+        else if(h.length < 10){
+            $(document).ajaxStart(function(){
+                $("#loading").css("display", "block");
+            });
+
+           $(document).ajaxComplete(function(){
+                $("#loading").css("display", "none");
+            });
+
+            $("#s_form").submit();
+        }
+    // });
+});
 });
