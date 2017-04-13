98fab814d969350059659e8f96e27aea61739ed8
Shabbir Ahmed <shabbirahmed149@gmail.com>
2017-04-13 05:30:36 +0000

    New Update

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
     
     validates_presence_of :full_name, :email, :password, :on => :create
+    validates_uniqueness_of :email,  :on => :create
+    validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
     has_many :foods
 
     mount_uploader :image, AvatarUploader
