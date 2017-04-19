034c6b20dc792a54f456ff362a6240e61461384f
Shabbir Ahmed <shabbirahmed149@gmail.com>
2017-04-19 06:59:00 +0000

    New

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+  # Flash message key
   add_flash_types :success, :warning, :danger, :info
   protect_from_forgery with: :exception
   include AccountsHelper
